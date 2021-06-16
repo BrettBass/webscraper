de9d794a86c974197589c8aa5060203ecc7131b8
Brett Bass <bbass@ucsd.edu>
2021-06-16 12:32:33 -0700

    variable names

--- webScrape.py
+++ webScrape.py
@@ -1,5 +1,11 @@
 import pdfkit as pdf
 
+#where bot saves the file
+directory = "C:Users/UserName/website.pdf"
+
+pdfName = "website.pdf"
+cheggLogin = 'https://www.chegg.com/auth?action=login&redirect=https%3A%2F%2Fwww.chegg.com%2F'
+
 def fileUpload(ctx, url):
     pdf.from_url(url, 'website.pdf', configuration=config)
-    await ctx.send(file=discord.File('C:/Users/UserName/website.pdf'))
+    await ctx.send(file=discord.File('directory'))
