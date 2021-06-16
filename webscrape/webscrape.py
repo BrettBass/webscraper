3ebdb243f30817132d47fd84cc2c318e15d8944a
Wtfitsaduck <Wtfitsaduck@gmail.com>
2021-06-16 13:32:48 -0700

    added linux and windows temp paths

--- webscrape/webscrape.py
+++ webscrape/webscrape.py
@@ -1,6 +1,8 @@
 import redbot.core
 import discord
 import pdfkit as pdf
+from sys import platform
+import os
 
 
 class webScrape(commands.Cog):
@@ -8,8 +10,12 @@ class webScrape(commands.Cog):
         __version__=[1.0]
     @commands.command(usage=["<URL>"])
     async def webscrape(self, ctx, url):
-    # where bot saves the file(probably gonna be running in linux, so we'll use linux temp directory)
-    save-directory = "/tmp/"
+    # where bot saves the file
+    if platform == "linux":
+        save-directory = "/tmp/"
+    elif platform == "win32"
+        running-user = os.getlogin()
+        save-directory = f"C:\\Users\\{running-user}\\Appdata\\Local\\Temp\\"
 
     pdf-name = "website.pdf"
     chegg-login = 'https://www.chegg.com/auth?action=login&redirect=https%3A%2F%2Fwww.chegg.com%2F'
