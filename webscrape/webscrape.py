05e337d4169fa85db9a8bd8241e231a1109370d8
Wtfitsaduck <Wtfitsaduck@gmail.com>
2021-06-16 13:23:20 -0700

    Changed some variables and fixed the file send

--- webscrape/webscrape.py
+++ webscrape/webscrape.py
@@ -6,13 +6,13 @@ import pdfkit as pdf
 class webScrape(commands.Cog):
         __author__=["Diddly", "Wtitsaduck"]
         __version__=[1.0]
-    @commands.command()
+    @commands.command(usage=["<URL>"])
     async def webscrape(self, ctx, url):
-    #where bot saves the file
-    directory = "C:Users/UserName/website.pdf"
+    # where bot saves the file(probably gonna be running in linux, so we'll use linux temp directory)
+    save-directory = "/tmp/"
 
-    pdfName = "website.pdf"
-    cheggLogin = 'https://www.chegg.com/auth?action=login&redirect=https%3A%2F%2Fwww.chegg.com%2F'
+    pdf-name = "website.pdf"
+    chegg-login = 'https://www.chegg.com/auth?action=login&redirect=https%3A%2F%2Fwww.chegg.com%2F'
 
     pdf.from_url(url, 'website.pdf', configuration=config)
-    #await ctx.send(file=discord.File('directory'))
+    await ctx.send(file=discord.File("directory" + "pdf-name"))
